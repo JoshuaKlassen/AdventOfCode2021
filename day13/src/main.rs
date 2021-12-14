16b2c2e6e45465c3320b86b3feaa79fd037b6ae0
JoshuaKlassen <joshuaklassen17@gmail.com>
2021-12-14 14:30:15 -0600

    Refactored solution

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,6 +1,41 @@
 use std::{io::{self, Read}, str::FromStr};
 use std::cmp;
 
+enum Axis {
+    X,
+    Y,
+}
+
+struct Command {
+    axis: Axis,
+    line: usize,
+}
+
+impl FromStr for Command {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        let mut tokens = s.split_whitespace();
+        tokens.next();
+        tokens.next();
+
+        let command = tokens.next().expect("Error reading command");
+        let mut command = command.split("=");
+        let axis_str = command.next().expect("Error reading axis");
+        let mut axis: Axis = Axis::X;
+        match axis_str {
+            "x" => (),
+            "y" => axis = Axis::Y,
+            _ => return Err(String::from("Could not parse axis"))
+        }
+
+        let line = command.next().expect("Error reading coordinate");
+        let line: usize = line.parse().expect("Error parsing coordinate");
+
+        Ok(Command {axis, line})
+    }
+}
+
 struct Point {
     x: i32,
     y: i32,
@@ -19,156 +54,146 @@ impl FromStr for Point {
     }
 }
 
+struct Manual {
+    paper: Vec<Vec<bool>>,
+}
+
+impl FromStr for Manual {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut result = Vec::<Vec<bool>>::new();
+        let mut points = Vec::<Point>::new();
+    
+        let mut largest_x = 0;
+        let mut largest_y = 0;
+    
+        for line in s.lines() {
+            let point: Point = line.trim().parse().expect("Error reading point");
+            largest_x = cmp::max(largest_x, point.x);
+            largest_y = cmp::max(largest_y, point.y);
+            points.push(point);
+        }
+    
+        for _i in 0..largest_y + 1 {
+            let mut row = Vec::<bool>::new();
+            for _j in 0..largest_x + 1{
+                row.push(false);
+            }
+            result.push(row);
+        }
+    
+        for point in points {
+            result[point.y as usize][point.x as usize] = true;
+        }
+    
+        Ok(Manual {paper: result})   
+    }
+}
+
+impl Manual {
+    fn fold(&mut self, command: &Command) {
+        match command.axis {
+            Axis::X => self.fold_paper_vertical(command.line),
+            Axis::Y => self.fold_paper_horizontal(command.line),
+        }
+    }
+
+    fn fold_paper_horizontal(&mut self, y: usize) {
+        if y > self.paper.len() {
+            panic!("Cannot fold paper beyond bounds! Paper size: {}x{}, requested fold along y={}", self.paper.len(), self.paper.first().unwrap().len(), y);
+        }
+    
+        let mut row_index = 0;
+        for i in (y+1..self.paper.len()).rev() {
+            for j in 0..self.paper[i].len() {
+                let value = self.paper[i][j];
+                if value {
+                    self.paper[row_index][j] = true;
+                }
+            }
+            row_index += 1;
+            self.paper.pop();
+        }
+        self.paper.pop();
+    }
+
+    fn fold_paper_vertical(&mut self, x: usize) {
+        if x > self.paper.first().unwrap().len() {
+            panic!("Cannot fold paper beyond bounds! Paper size: {}x{}, requested fold along x={}", self.paper.len(), self.paper.first().unwrap().len(), x);
+        }
+    
+        for i in 0..self.paper.len() {
+            let mut col_index = 0;
+            for _j in (x+1..self.paper[i].len()).rev() {
+                let value = self.paper[i].pop().expect("Error reading paper");
+                if value {
+                    self.paper[i][col_index] = true;
+                }
+                col_index += 1;
+            }
+            self.paper[i].pop();
+        }
+    }
+
+    fn print_paper(&self) {
+        for row in &self.paper {
+            for col in row {
+                if *col {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("");
+        }
+    }
+    
+    fn count_points_on_paper(&self) -> i32 {
+        let mut count = 0;
+    
+        for row in &self.paper {
+            for col in row {
+                if *col {
+                    count += 1;
+                }
+            }
+        }
+    
+        count
+    }
+}
+
 fn part_one(input: &str) -> i32 {
     let mut tokens = input.split("\n\n");
-    let paper = tokens.next().expect("Failed reading paper");
-    let mut paper = parse_paper(&paper);
+    let manual = tokens.next().expect("Failed reading paper");
+    let mut manual: Manual = manual.parse().expect("Error parsing manual");
     
     let commands = tokens.next().expect("Failed to parse commands");
     let mut commands = commands.split("\n");
 
     let command = commands.next().expect("Error reading command");
-    let mut tokens = command.split_whitespace();
-    tokens.next();
-    tokens.next();
-    let line = tokens.next().expect("Error reading command");
-    let mut line = line.split("=");
-    let axis = line.next().expect("Error reading axis");
-    let axis: char = axis.parse().expect("Error parsing axis");
-    let coord = line.next().expect("Error reading coordinate");
-    let coord: usize = coord.parse().expect("Error parsing coordinate");
-
-    match axis {
-        'x' => fold_paper_vertical(&mut paper, coord),
-        'y' => fold_paper_horizontal(&mut paper, coord),
-        _ => {}
-    }
+    let command: Command = command.parse().expect("Error parsing command");
+    manual.fold(&command);
     
-    count_points_on_paper(&paper)
-}
-
-fn parse_paper(input: &str) -> Vec<Vec<bool>> {
-    let mut result = Vec::<Vec<bool>>::new();
-    let mut points = Vec::<Point>::new();
-
-    let mut largest_x = 0;
-    let mut largest_y = 0;
-
-    for line in input.lines() {
-        let point: Point = line.trim().parse().expect("Error reading point");
-        largest_x = cmp::max(largest_x, point.x);
-        largest_y = cmp::max(largest_y, point.y);
-        points.push(point);
-    }
-
-    for _i in 0..largest_y + 1 {
-        let mut row = Vec::<bool>::new();
-        for _j in 0..largest_x + 1{
-            row.push(false);
-        }
-        result.push(row);
-    }
-
-    for point in points {
-        result[point.y as usize][point.x as usize] = true;
-    }
-
-    result
+    manual.count_points_on_paper()
 }
 
 fn part_two(input: &str) {
     let mut tokens = input.split("\n\n");
-    let paper = tokens.next().expect("Failed reading paper");
-    let mut paper = parse_paper(&paper);
+    let manual = tokens.next().expect("Failed reading paper");
+    let mut manual: Manual = manual.parse().expect("Error parsing manual");
     
     let commands = tokens.next().expect("Failed to parse commands");
     let commands = commands.split("\n");
 
     for command in commands {
-        let command = command.trim();
-        let mut tokens = command.split_whitespace();
-        tokens.next();
-        tokens.next();
-        let line = tokens.next().expect("Error reading command");
-        let mut line = line.split("=");
-        let axis = line.next().expect("Error reading axis");
-        let axis: char = axis.parse().expect("Error parsing axis");
-        let coord = line.next().expect("Error reading coordinate");
-        let coord: usize = coord.parse().expect("Error parsing coordinate");
-    
-        match axis {
-            'x' => fold_paper_vertical(&mut paper, coord),
-            'y' => fold_paper_horizontal(&mut paper, coord),
-            _ => {}
-        }
+        let command: Command = command.parse().expect("Error parsing command");
+        manual.fold(&command);
     }
 
-    print_paper(&paper);
+    manual.print_paper();
 }
 
-fn fold_paper_horizontal(paper: &mut Vec<Vec<bool>>, y: usize) {
-    if y > paper.len() {
-        panic!("Cannot fold paper beyond bounds! Paper size: {}x{}, requested fold along y={}", paper.len(), paper.first().unwrap().len(), y);
-    }
-
-    let mut row_index = 0;
-    for i in (y+1..paper.len()).rev() {
-        for j in 0..paper[i].len() {
-            let value = paper[i][j];
-            if value {
-                paper[row_index][j] = true;
-            }
-        }
-        row_index += 1;
-        paper.pop();
-    }
-    paper.pop();
-}
-
-fn fold_paper_vertical(paper: &mut Vec<Vec<bool>>, x: usize) {
-    if x > paper.first().unwrap().len() {
-        panic!("Cannot fold paper beyond bounds! Paper size: {}x{}, requested fold along x={}", paper.len(), paper.first().unwrap().len(), x);
-    }
-
-    for i in 0..paper.len() {
-        let mut col_index = 0;
-        for _j in (x+1..paper[i].len()).rev() {
-            let value = paper[i].pop().expect("Error reading paper");
-            if value {
-                paper[i][col_index] = true;
-            }
-            col_index += 1;
-        }
-        paper[i].pop();
-    }
-}
-
-fn print_paper(paper: &Vec<Vec<bool>>) {
-    for row in paper {
-        for col in row {
-            if *col {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!("");
-    }
-}
-
-fn count_points_on_paper(paper: &Vec<Vec<bool>>) -> i32 {
-    let mut count = 0;
-
-    for row in paper {
-        for col in row {
-            if *col {
-                count += 1;
-            }
-        }
-    }
-
-    count
-}
 
 fn main() {
     let mut buffer = String::new();
@@ -202,13 +227,14 @@ fn test_parse_paper() {
 8,10
 9,0");
 
-    let paper = parse_paper(&input);
+    let manual: Manual = input.parse().unwrap(); 
+    let paper = &manual.paper;
 
     assert_eq!(15, paper.len());
     assert_eq!(11, paper.first().unwrap().len());
     assert_eq!(true, paper[0][3]);
-    assert_eq!(18, count_points_on_paper(&paper));
-    print_paper(&paper);
+    assert_eq!(18, manual.count_points_on_paper());
+    manual.print_paper()
 }
 
 #[test]
@@ -232,13 +258,12 @@ fn test_fold_paper() {
 8,10
 9,0");
 
-    let mut paper = parse_paper(&input);
+    let mut manual: Manual = input.parse().unwrap(); 
+    manual.fold_paper_horizontal(7);
+    assert_eq!(17, manual.count_points_on_paper());
 
-    fold_paper_horizontal(&mut paper, 7);
-    assert_eq!(17, count_points_on_paper(&paper));
-
-    fold_paper_vertical(&mut paper, 5);
-    print_paper(&paper);
+    manual.fold_paper_vertical(5);
+    manual.print_paper();
 }
 
 #[test]
